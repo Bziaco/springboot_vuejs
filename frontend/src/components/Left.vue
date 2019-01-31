ed0457608525bd94e7c486ddbb061ce7d14aec12
bziaco <bjh9222@naver.com>
2019-01-31 18:18:23 +0900

    vue.js tree 연동

---
+++ frontend/src/components/Left.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="w-100 p-2" style="background-color: rgba(0, 0, 0, 0.03)">
+      Browser
+    </div>
+    <v-jstree :data="data" allow-batch whole-row draggable @item-click="itemClick" ref="tree"></v-jstree>
+  </div>
+</template>
+
+<script>
+  import VJstree from 'vue-jstree'
+  export default {
+    name: 'app',
+    components: {
+      VJstree
+    },
+    data() {
+      return {
+        data: [{
+          "text": "Server",
+          "children": [
+            {
+              "text": "OracleSql"
+            },
+            {
+              "text": "MS Sql"
+            },
+            {
+              "text": "MySql",
+              "opened": true,
+              "children": [
+                {
+                  "text": "Databases",
+                  "children": [
+                    {
+                      "text": "wdtdb"
+                    },
+                    {
+                      "text": "wdttestdb"
+                    },
+                    {
+                      "text": "wdtpsqldb"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
