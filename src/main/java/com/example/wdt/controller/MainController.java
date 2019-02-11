8a0c3956de71103871d63197c99727dc0bcc17df
bziaco <bjh9222@naver.com>
2019-02-11 13:30:18 +0900

    logger 추가

---
+++ src/main/java/com/example/wdt/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.example.wdt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class MainController {
+    @GetMapping("test")
+    public String test() {
+        return "start platformance, fighting!";
+    }
+}
