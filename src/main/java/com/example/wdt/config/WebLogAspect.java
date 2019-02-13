3e6a0137bba68cefdb7f0f03a37fa3f371911df7
bziaco <bjh9222@naver.com>
2019-02-13 10:05:33 +0900

    db 연결 테스트

--- src/main/java/com/example/wdt/config/WebLogAspect.java
+++ src/main/java/com/example/wdt/config/WebLogAspect.java
@@ -1,6 +1,7 @@
 package com.example.wdt.config;
 
 import org.apache.log4j.Logger;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
--- src/main/java/com/example/wdt/controller/MainController.java
+++ src/main/java/com/example/wdt/controller/MainController.java
@@ -5,11 +5,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @RestController
 @RequestMapping("api")
 public class MainController {
     @GetMapping("test")
     public String test(@RequestParam(value = "title")String title) {
-        return title + "Spring boot 공부 합시다!!!!";
+
+        //SpringApplication.run(WdtApplication.class, args);
+        // Create a variable for the connection string.
+        String connectionUrl = "jdbc:sqlserver://dbpadinstance.cdpfajomg88b.ap-northeast-2.rds.amazonaws.com:1433;databaseName=DBPAD_DEV_MEMBER;user=myuser;password=1q2w3e4r";
+        String result = "";
+        try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
+            String SQL = "SELECT TOP 10 * FROM [DBPAD_DEV_MEMBER].[dbo].[TB_Member_Info]";
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            // Iterate through the data in the result set and display it.
+            while (rs.next()) {
+                result += rs.getString("Member_Number") + " " + rs.getString("ID");
+            }
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
